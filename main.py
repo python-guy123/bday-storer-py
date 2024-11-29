81afd3a5687e2d721e27b5e163fabd567ef3a990
python-guy123 <vaishnav.sreekiran@gmail.com>
2024-11-29 11:13:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+john_name = "John"
+john_bday = "10-12-13"
+
+ravi_name = "Ravi"
+ravi_bday = "30-2-10"
+
+jack_name = "Jack"
+jack_bday = "26-8-12"
+
+dan_name = "Dan"
+dan_bday = "14-12-11"
+
+raj_name = "Raj"
+raj_bday = "25-6-17"
+
+print(john_name , john_bday)
+print(ravi_name , ravi_bday)
+print(jack_name , jack_bday)
+print(dan_name , dan_bday)
+print(raj_name, raj_bday)
